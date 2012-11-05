cc94e32bd108b408cd67266e4c3d66d523163b5f
Y.Okamura <okamura@informationsea.info>
2012-11-05 23:56:05 +0900

    update setup information

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from distutils.core import setup
 
 setup(name='linkdown',
       version='0.1',
-      py_modules=['linkdown'],
+      description='Build Webpage with Markdown, Jinja2 templates, LESS or some recent tools.',
+      packages=['linkdown'],
+      author='Y.Okamura',
+      license='GPL3+',
+      url='https://github.com/informationsea/linkdown',
+      install_requires = ['Markdown>=2.2.0', 'Jinja2>=2.6', 'watchdog>=0.6.0'],
       scripts=['scripts/linkdown']
       )
